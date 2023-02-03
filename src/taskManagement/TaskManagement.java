7f516f6b6023857516c024e3f5be120bfb57ab47
bp21060 <bp21060@shibaura-it.ac.jp>
2023-02-03 17:30:34 +0900

    課題の追加するボタンの作成

--- src/taskManagement/TaskManagement.java
+++ src/taskManagement/TaskManagement.java
@@ -4,20 +4,26 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TaskManagement extends JFrame {
-
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					TaskManagement frame = new TaskManagement();
@@ -36,21 +42,68 @@ public class TaskManagement extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		JPanel p = new JPanel();
-		GridLayout layout = new GridLayout(1,2);
+		GridLayout layout = new GridLayout(1, 2);
 		layout.setHgap(150);
-		
+		Container contentPane = getContentPane();
+
 		//追加、削除ボタン
 		JButton addButton = new JButton("追加");
 		JButton deleteButton = new JButton("削除");
-		
+
 		p.setLayout(layout);
 		p.add(addButton);
 		p.add(deleteButton);
 
-		
-		
-		Container contentPane = getContentPane();
-		contentPane.add(p,BorderLayout.PAGE_END);
+		contentPane.add(p, BorderLayout.PAGE_END);
+
+		//タスクの表示
+		GroupLayout layout2 = new GroupLayout(contentPane);
+		contentPane.setLayout(layout2);
+
+		//間隔開ける
+		layout2.setAutoCreateGaps(true);
+		layout2.setAutoCreateContainerGaps(true);
+
+		List<JComponent> taskList = new ArrayList<>();
+
+		//デバック
+		int taskAmount = 20;
+
+		for (int i = 0; i < taskAmount; i++) {
+			JComponent label = new JLabel("課題" + i);
+			JComponent detailButton = new JButton("詳細");
+			JComponent completeButton = new JButton("完了");
+			taskList.add(label);
+			taskList.add(detailButton);
+			taskList.add(completeButton);
+		}
+
+		//水平グループ
+		SequentialGroup hGroup = layout2.createSequentialGroup();
+
+		for (int i = 0; i < 3; i++) {
+			ParallelGroup pGroup = layout2.createParallelGroup();
+			for (int j = 0; j < taskAmount; j++) {
+				pGroup.addComponent(taskList.get(3 * j + i));
+			}
+			hGroup.addGroup(pGroup);
+		}
+
+		layout2.setHorizontalGroup(hGroup);
+
+		//垂直グループ
+		SequentialGroup vGroup = layout2.createSequentialGroup();
+
+		for (int i = 0; i < taskAmount; i++) {
+			ParallelGroup pGroup2 = layout2.createParallelGroup();
+			for (int j = 0; j < 3; j++) {
+				pGroup2.addComponent(taskList.get(3 * i + j));
+			}
+			vGroup.addGroup(pGroup2);
+		}
+
+		layout2.setVerticalGroup(vGroup);
+
 	}
 
 }
