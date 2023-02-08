6627c11e8edb233551edbf38f58c48291af89e69
bp21060 <bp21060@shibaura-it.ac.jp>
2023-02-08 16:39:38 +0900

    画面の切り替え操作の追加

--- src/taskManagement/TaskManagement.java
+++ src/taskManagement/TaskManagement.java
@@ -1,16 +1,22 @@
 package taskManagement;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public class TaskManagement extends JFrame {
+public class TaskManagement extends JFrame implements ActionListener {
+
+	JPanel cardLayoutPanel;
+	CardLayout cardLayout;
 
 	/**
 	 * Launch the application.
@@ -36,27 +42,47 @@ public class TaskManagement extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 
-		JPanel p = new JPanel();
-		GridLayout layout = new GridLayout(1, 2);
-		layout.setHgap(100);
-		Container contentPane = getContentPane();
-
-		//追加、削除ボタンのパネルを追加
-		JButton addButton = new JButton("課題の追加");
-		JButton deleteButton = new JButton("完了した課題の削除");
-
-		p.setLayout(layout);
-		p.add(addButton);
-		p.add(deleteButton);
-
 		//課題一覧パネルを追加
 		TaskListPanel taskListPanel = new TaskListPanel();
 		JScrollPane taskScroll = new JScrollPane(taskListPanel.MakePanel());
 		taskScroll.getVerticalScrollBar().setUnitIncrement(20);
 
+		//課題追加パネルを追加
+		JPanel taskAddPanel = new JPanel();
+
+		//画面遷移
+		cardLayoutPanel = new JPanel();
+		cardLayout = new CardLayout();
+		cardLayoutPanel.setLayout(cardLayout);
+
+		cardLayoutPanel.add(taskScroll, "taskList");
+		cardLayoutPanel.add(taskAddPanel, "taskAdd");
+
+		//追加、削除ボタンのパネルを追加
+		JPanel p = new JPanel();
+		GridLayout layout = new GridLayout(1, 2);
+		layout.setHgap(100);
+		Container contentPane = getContentPane();
+
+		JButton addButton = new JButton("課題の追加");
+		addButton.addActionListener(this);
+		addButton.setActionCommand("taskAdd");
+
+		JButton deleteButton = new JButton("完了した課題の削除");
+		p.setLayout(layout);
+		p.add(addButton);
+		p.add(deleteButton);
+
 		//削除追加ボタンパネルと課題一覧パネルをcontentPaneに追加
 		contentPane.add(p, BorderLayout.PAGE_END);
-		contentPane.add(taskScroll, BorderLayout.CENTER);
+		contentPane.add(cardLayoutPanel, BorderLayout.CENTER);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
+		String command = actionEvent.getActionCommand();
+
+		cardLayout.show(cardLayoutPanel, command);
 	}
 
 }
